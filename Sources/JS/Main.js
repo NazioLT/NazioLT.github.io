fde176146ecfb320cb9633be8dc7bab9510def01
Nazio_LT <zoupeurletroll@gmail.com>
2022-10-20 11:42:13 +0200

    Editing Index

--- Sources/JS/Main.js
+++ Sources/JS/Main.js
@@ -4,6 +4,6 @@ function Button() {
 }
 
 window.addEventListener('scroll', () => {
-    let y = (1 + (window.scrollY || window.pageYOffset)) / 400
+    let y = (1 + (window.scrollY || window.pageYOffset)) / 750
     document.getElementById('Header').style.backgroundColor = `rgba(${56}, ${56}, ${56}, ${y})`;
 })
